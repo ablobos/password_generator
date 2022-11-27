77ead11cdd8b9ae7d18cb1eef4e38ced4da3baba
Abdiel "A.B." Villalobos <ablobosportfolio@gmail.com>
2022-11-27 16:40:36 -0500

    first commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,22 @@
 // Assignment Code
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharArr = [];
+var lowerCaseArr = [];
+var upperCaseArr = [];  
+var numerArr = [];
+
+
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("Test: Generate Button Clicked")
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +27,7 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+
+}
+

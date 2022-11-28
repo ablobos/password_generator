6ef10a22e3e633a8ed56363884001c948d1ca327
Abdiel "A.B." Villalobos <ablobosportfolio@gmail.com>
2022-11-28 06:06:05 -0500

    third commit

--- Develop/script.js
+++ Develop/script.js
@@ -2,10 +2,10 @@
 var characterLength = 8;
 var choiceArr = [];
 
-var specialCharArr = [];
-var lowerCaseArr = [];
-var upperCaseArr = [];  
-var numerArr = [];
+var specialCharArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?"];
+var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];  
+var numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
 var generateBtn = document.querySelector("#generate");
@@ -28,6 +28,16 @@ function writePassword() {
 }
 
 function generatePassword() {
-
+  //genreatePassword based on the prompts
 }
 
+function getPrompts() {
+  characterLength= parseIntprompt("How many characters would you like your password to become? (must be between 8 - 128 characters");
+
+  if(isNaN(characterLength) ││ characterLength < 8 ││ characterLength > 128)
+    alert(Please only choose between 8 - 128 DOMStringList. Please try again.);
+    return false; 
+}
+
+confirm("Would you like lowercase letters in your password?");
+  choiceArr = choiceArr.concat(lowerCaseArr); 
